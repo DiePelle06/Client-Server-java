bc814a4a1f85da8eb7417da6522eda2319d44f54
AlessioRicci699 <s8901717u@majorana-giorgi.edu.it>
2024-11-29 10:59:20 +0100

    Creazioone Client Java

--- ClientJava/src/Main.java
+++ ClientJava/src/Main.java
@@ -1,9 +1,27 @@
-//client
-public class Main {
+import java.io.*;
+import java.net.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Client {
+    public static void main(String args[]) throws IOException
+    {
+        // create a socket to connect to the server running on localhost at port number 9090
+        Socket socket = new Socket("10.130.1.124", 31124);
+       
+        // Setup output stream to send data to the server
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+       
+        // Setup input stream to receive data from the server
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-	}
+        // Send message to the server
+        out.println("Hello from client!");
 
+        // Receive response from the server
+        String response = in.readLine();
+        System.out.println("Server says: " + response);
+
+        // Close the socket
+        socket.close();
+    }
 }
+
