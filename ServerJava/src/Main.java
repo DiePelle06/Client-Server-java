41677db9e3c294417fb0f8ce0e7de267923e8ee5
DiePelle06 <s8901715q@majorana-giorgi.edu.it>
2024-11-29 10:57:42 +0100

    server

--- ServerJava/src/Main.java
+++ ServerJava/src/Main.java
@@ -1,9 +1,41 @@
 //server
+
+import java.io.*;
+import java.net.*;
+
 public class Main 
 {
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		// create a server socket on port number 9090
+		try
+		{
+			ServerSocket serverSocket = new ServerSocket(9090);
+			System.out.println("Server is running and waiting for client connection...");
 
+			// Accept incoming client connection
+			Socket clientSocket = serverSocket.accept();
+			System.out.println("Client connected!");
+
+			// Setup input and output streams for communication with the client
+			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+			// Read message from client
+			String message = in.readLine();
+			System.out.println("Client says: " + message);
+
+			// Send response to the client
+			out.println("Message received by the server.");
+
+			// Close the client socket
+			clientSocket.close();
+			// Close the server socket
+			serverSocket.close();
+		}
+		catch (IOException e)
+		{
+			System.out.println(e);
+		}
 	}
 }
